167ff3ba3770934c5623ca823ce401d7621edc9c
Gissele222 <149848476+Gissele222@users.noreply.github.com>
2024-03-06 20:35:36 -0800

    integrated API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function createPoem(event) {
   event.preventDefault();
 
-  let poemElement = document.querySelector("#poem");
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "1c608c83aofd04et93b4143f29525423";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write. Your mission is to generate a 5 line poem in basic HTML and separate each line. Do not include a title to the poem.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let submitButton = document.querySelector("#submit-button");
+  submitButton.setAttribute("disabled", "disabled");
 
-  new Typewriter("#poem", {
-    strings: "I like to write poems",
-    autoStart: true,
-    delay: 1,
-    cursos: "",
-  });
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-creator-form");
